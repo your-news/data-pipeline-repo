ed814d20b7d2e0d1e4a6754e949f6a645f676e14
Moveuk <v_donguk@naver.com>
2022-09-23 16:29:48 +0900

    fix: #6 디렉토리 내부의 파일 순환하며 알맞은 index 생성하여 csv bulk data 삽입

--- ImportCSV2ES/import_csv.py
+++ ImportCSV2ES/import_csv.py
@@ -1,14 +1,31 @@
 from elasticsearch import helpers, Elasticsearch
+import os
 import csv
+import time
+import ctypes as ct
 
 
 es = Elasticsearch(['http://elasticsearch-server-ip:9200/'], port=9200)
 
-if es.indices.exists(index="news-bulk"):
-  pass
-else:
-  es.indices.create(index="news-bulk")
+# csv 파일 디렉토리 변수 초기화
+dir_path = "E:\\news2"
 
-with open('D:/IT/Project/KoreaNewsCrawler-master/output/Article_IT과학_20200101_20200131.csv', encoding='UTF8') as f:
-  reader = csv.DictReader(f)
-  helpers.bulk(es, reader, index="news-bulk", raise_on_error=False)
+# 디렉토리 내부 순환
+for (root, directories, files) in os.walk(dir_path):
+    # csv 파일 순환
+    for file in files:
+        # csv 파일 명 쪼개어 index 명으로 변경
+        # news_IT과학_20140201_20140228 -> news-IT과학-201402
+        file_path = os.path.join(root, file).split("_")
+        indexname = "news-" + file_path[1] + "-" + file_path[2][0:6]
+        print(indexname)
+
+        # ES 내부 index 존재 여부 확인
+        if es.indices.exists(index=indexname):
+          pass
+        else:
+          es.indices.create(index=indexname)
+        # csv파일 열어 Bulk insert
+        with open(file_path, encoding='UTF8') as f:
+          reader = csv.DictReader(f)
+          helpers.bulk(es, reader, index=indexname, raise_on_error=False)
