2aebea082841a414eced8e1c8e86eeac45137a5d
Moveuk <v_donguk@naver.com>
2022-09-23 16:33:41 +0900

    fix: #6 삽입 테스트 시간 측정 console print문 추가

--- ImportCSV2ES/import_csv.py
+++ ImportCSV2ES/import_csv.py
@@ -4,12 +4,17 @@ import csv
 import time
 import ctypes as ct
 
-
 es = Elasticsearch(['http://elasticsearch-server-ip:9200/'], port=9200)
 
 # csv 파일 디렉토리 변수 초기화
 dir_path = "E:\\news2"
 
+# Bulk data 삽입 시간 측정
+before = round(time.time())
+print("데이터 삽입 전 시간 :", time.strftime('%c', time.localtime(before)))
+print("데이터 삽입 전 시간(초) :", before)
+
+i = 0 # 파일 갯수 확인용 변수
 # 디렉토리 내부 순환
 for (root, directories, files) in os.walk(dir_path):
     # csv 파일 순환
@@ -22,10 +27,19 @@ for (root, directories, files) in os.walk(dir_path):
 
         # ES 내부 index 존재 여부 확인
         if es.indices.exists(index=indexname):
-          pass
+            pass
         else:
-          es.indices.create(index=indexname)
+            es.indices.create(index=indexname)
         # csv파일 열어 Bulk insert
         with open(file_path, encoding='UTF8') as f:
-          reader = csv.DictReader(f)
-          helpers.bulk(es, reader, index=indexname, raise_on_error=False)
+            reader = csv.DictReader(f)
+            helpers.bulk(es, reader, index=indexname, raise_on_error=False)
+        # 파일 당 삽입 소요시간 측정
+        i += 1
+        print(i,"월 종료 소요시간 : ",(round(time.time())-before), "s")
+
+# 최종 소요시간 측정
+after = round(time.time())
+print("데이터 삽입 전 시간 :", time.strftime('%c', time.localtime(after)))
+print("데이터 삽입 전 시간(초) :", after)
+print("dif : ", (after-before), "s")
